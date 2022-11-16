af894955e579ad66b9eb02d081a42284b5e5c6cd
Gaston <liam@giffordclan.org>
2022-11-16 08:58:07 -0700

    timestamp update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('ASD Chinese Typing Test')
 console.log('Created by Liam Gifford')
-console.log('Last Updated 16 November | 08:36 AM')
+console.log('Last Updated 16 November | 08:57 AM')
 
 // SwitchPage Setup
 const switchBtns = document.getElementsByClassName('switch')
